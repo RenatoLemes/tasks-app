8638d9d7eb6734beed9839aec5883d1819bd4086
RenatoLemes <renato.lemmes@hotmail.com>
2023-08-07 09:02:25 +0000

    fixing NavBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
+import React from "react";
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
-import { Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./api/axiosDefaults";
-import { TasksList } from "./components/TasksList";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
-        <TasksList state="Open" />
-        <TasksList state="InPogress" />
-        <TasksList state="Done" />
-        {/* <Routes>
-          <Route path="/open" element={<h1>Open</h1>} />
-          <Route path="/in-progress" element={<h1>In Progress</h1>} />
-          <Route path="/done" element={<h1>Done</h1>} />
-        </Routes> */}
+        <Routes>
+          <Route exact path="/open" render={() => <h1>Open</h1>} />
+          <Route exact path="/in-progress" render={() => <h1>In Progress</h1>} />
+          <Route exact path="/done" render={() =><h1>Done</h1>} />
+        </Routes>
       </Container>
     </div>
   );
---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://task-app-renato-bb143324df86.herokuapp.com";
+
+export default axios;
+
+// http://127.0.0.1:8000/api
